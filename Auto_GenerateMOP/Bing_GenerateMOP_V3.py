e02bf8838750044d2b5aa155049f1ea985523154
engkufizz <59971168+engkufizz@users.noreply.github.com>
2023-02-16 11:03:22 +0800

    Add files via upload

---
+++ Auto_GenerateMOP/Bing_GenerateMOP_V3.py
@@ -0,0 +1,60 @@
+from pathlib import Path  # core python module
+import win32com.client  # pip install pywin32
+import ast  # core python module
+
+# Path settings
+current_dir = Path.cwd()
+input_file = current_dir / "Data.docx"
+output_file = current_dir / "New_Data.docx"
+changelist_file = current_dir / "Changelist.txt"
+
+# Read the keywords and replacements from the text file
+with open(changelist_file, "r") as f:
+    replacements = ast.literal_eval(f.read())
+
+# Replace keywords and their replacements
+wd_replace = 2  # 2=replace all occurrences, 1=replace one occurrence, 0=replace no occurrences
+wd_find_wrap = 1  # 2=ask to continue, 1=continue search, 0=end if search range is reached
+
+# Open Word
+word_app = win32com.client.DispatchEx("Word.Application")
+word_app.Visible = False
+word_app.DisplayAlerts = False
+
+# Open the input document and replace keywords
+word_app.Documents.Open(str(input_file))
+# Loop through the list of replacements
+for keyword, replacement in replacements:
+    # API documentation: https://learn.microsoft.com/en-us/office/vba/api/word.find.execute
+    word_app.Selection.Find.Execute(
+        FindText=keyword,
+        ReplaceWith=replacement,
+        Replace=wd_replace,
+        Forward=True,
+        MatchCase=True,
+        MatchWholeWord=False,
+        MatchWildcards=True,
+        MatchSoundsLike=False,
+        MatchAllWordForms=False,
+        Wrap=wd_find_wrap,
+        Format=True,
+    )
+
+    # Replace the keywords in shapes
+    # VBA SO reference: https://stackoverflow.com/a/26266598
+    # Loop through all the shapes
+    for i, shape in enumerate(word_app.ActiveDocument.Shapes):
+        if shape.TextFrame.HasText:
+            words = shape.TextFrame.TextRange.Words
+            # Loop through each word. This method preserves formatting.
+            for j, word in enumerate(words):
+                # If a word exists, replace the text of it, but keep the formatting.
+                if word.Text == keyword:
+                    word.Text = replacement
+
+# Save the new file
+word_app.ActiveDocument.SaveAs(str(output_file))
+word_app.ActiveDocument.Close(SaveChanges=False)
+
+# Quit Word
+word_app.Application.Quit()
