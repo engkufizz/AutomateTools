494eeb1c5e7f0174ad826fcf851308750b65888f
engkufizz <59971168+engkufizz@users.noreply.github.com>
2023-06-14 11:36:54 +0800

    Add files via upload
    The previous version may have an error.

---
+++ ExcelAnalyzer_Tools/Clean_VersionDomain_V2.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# read in the Excel file
+df = pd.read_excel('raw_data.xlsx', skiprows=3)
+
+# remove the text within the brackets
+df['Software Version'] = df['Software Version'].str.replace(r'\(.*\)', '', regex=True)
+
+# extract the required string using regular expression
+df['Software Version'] = df['Software Version'].str.extract(r'(V\d+R\d+C\d+SPC\d+)')
+
+# Replace 'ROOT/' and everything after '/' in column 'E' with an empty string
+df['Subnet Path'] = df['Subnet Path'].str.replace('ROOT/', '').str.split('/').str[0]
+
+# write the final modified data to a new Excel file
+df.to_excel('clean_data.xlsx', index=False)
