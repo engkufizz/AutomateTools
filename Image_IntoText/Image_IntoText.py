cc5a30feed74877cad6cd41584bb2c712dede1d8
engkufizz <59971168+engkufizz@users.noreply.github.com>
2023-06-15 12:10:40 +0800

    Add files via upload

---
+++ Image_IntoText/Image_IntoText.py
@@ -0,0 +1,25 @@
+import easyocr
+from PIL import Image
+import numpy as np
+
+# Install easyocr using pip
+# pip install easyocr
+
+# Define the OCR reader and specify the language(s) to use
+reader = easyocr.Reader(['en'])
+
+# Open the image file
+image_path = r'image.jpg'
+image = Image.open(image_path)
+
+# Convert the image to a NumPy array
+image_np = np.array(image)
+
+# Perform OCR on the image
+result = reader.readtext(image_np)
+
+# Extract the text from the result
+text = ' '.join([res[1] for res in result])
+
+# Print the extracted text
+print(text)
